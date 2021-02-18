26e656fdfd134f464e7ebf2f08da30c8a763d3bb
garth <gmhull@wpi.edu>
2021-02-17 22:00:23 -0500

    Fixing cards on mobile and adjusting projects page

--- garthdotcom/settings.py
+++ garthdotcom/settings.py
@@ -121,9 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'personal/static'),
-
+# Grab the AWS S3 bucket info from environment variables
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
@@ -132,15 +130,18 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 AWS_LOCATION = 'static'
+# Make all of the files we save hav epublic read access.  Allows the website to see the files.
 AWS_DEFAULT_ACL = 'public-read'
 
+# Point the site to the location of static files when collecting
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'personal/static'),
 ]
-STATIC_URL = '/static/'
+# Provide a url for static and media files
+# STATIC_URL = '/static/'
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 MEDIA_URL = 'https://%s/media/' % (AWS_S3_CUSTOM_DOMAIN)
 
+# This controles where files are stored
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
 DEFAULT_FILE_STORAGE = 'garthdotcom.storage_backends.MediaStorage'
--- projects/urls.py
+++ projects/urls.py
@@ -3,12 +3,6 @@ from django.urls import path, include, re_path
 from projects.models import Project
 from .views import ProjectListView, ProjectDetailView
 
-# urlpatterns = [
-#     path('', ListView.as_view(queryset=Project.objects.all().order_by("-date")[:25],template_name="projects/index.html")),
-#     # I want to change this to be the project title or tag I think
-#     re_path('<slug:slug>',DetailView.as_view(model=Project,template_name='projects/project.html'))
-# ]
-
 urlpatterns = [
     path('', ProjectListView.as_view(), name='project_list'),
     path('<slug:slug>', ProjectDetailView.as_view(), name='project_detail')
