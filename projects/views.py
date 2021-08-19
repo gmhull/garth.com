820e40bff57913456084a8e0e64187d58f2c4466
garth <gmhull@wpi.edu>
2021-08-18 21:24:01 -0400

    Sort the projects by date: newest to oldest

--- projects/views.py
+++ projects/views.py
@@ -4,6 +4,7 @@ from .models import Project
 class ProjectListView(ListView):
     model = Project
     template_name = "projects/index.html"
+    ordering = ['-date']
 
 class ProjectDetailView(DetailView):
     model = Project
