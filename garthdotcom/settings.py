e86448529feb51bf74920944182e3a7d5dfefa8c
garth <gmhull@wpi.edu>
2022-12-14 23:33:21 -0500

    edit allowed hosts

--- garthdotcom/settings.py
+++ garthdotcom/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['garthdotcom.herokuapp.com',
                  '127.0.0.1', 
                  '.garths.world', 
                  'env-garthdotcom.eba-9bprb3ie.us-east-1.elasticbeanstalk.com',
+                 '52.201.105.210',
                  ]
 
 SECURE_SSL_REDIRECT = False
