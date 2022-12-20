a972dfcf8806b244f1df15d0940ee89ebb8ac5a2
garth <gmhull@wpi.edu>
2022-12-19 22:08:51 -0500

    add createsuperuser command

--- garthdotcom/settings.py
+++ garthdotcom/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DJANGO_DEBUG') == "True")
 
-ALLOWED_HOSTS = ['*',
-                 'garthdotcom.herokuapp.com', 
+ALLOWED_HOSTS = ['garthdotcom.herokuapp.com', 
                  '.localhost', 
                  '127.0.0.1', 
                  '.garths.world',
