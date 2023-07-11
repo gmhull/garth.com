ed9dc3f9f2b73fd5e5686fe7a593553988ca6bd3
gmhull <ghullmcsweeney@gmail.com>
2023-07-11 00:14:25 +0000

    update wsgi

--- garthdotcom/wsgi.py
+++ garthdotcom/wsgi.py
@@ -7,9 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-import os
-
+import os, sys
 from django.core.wsgi import get_wsgi_application
+
+path = '/home/gmhull/garthdotcom'
+if path not in sys.path:
+    sys.path.insert(0, path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'garthdotcom.settings')
 
