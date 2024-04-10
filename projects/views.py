90eeea5b728817fc4c1ac085c6d79dbe41601eb1
Garth Hull <ghullmcsweeney@gmail.com>
2024-04-09 23:19:32 -0400

    fix wrong connection

--- projects/views.py
+++ projects/views.py
@@ -15,7 +15,7 @@ class ProjectListView(ListView):
 
 class ProjectDetailView(DetailView):
     model = Project
-    template_name = "projects/project_detail_alt.html"
+    template_name = "projects/project_detail.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
