3c12d6965d0ead15ba9dd50518de470e8e6cb6e9
Garth Hull <ghullmcsweeney@gmail.com>
2026-07-14 21:54:54 -0400

    update allowed hosts

--- garthdotcom/settings.py
+++ garthdotcom/settings.py
@@ -28,11 +28,12 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.getenv('DJANGO_DEBUG') == "True")
 
-ALLOWED_HOSTS = ['garthdotcom.herokuapp.com',
+ALLOWED_HOSTS = [
                  '.localhost',
                  '127.0.0.1',
                  '.garths.world',
                  'gmhull.pythonanywhere.com/',
+                 '.vercel.app',
                  ]
 
 SECURE_SSL_REDIRECT = False
